4214333bf3fb0cab9ccc4464cc127cf51a9f53da
Lee Barrett <90680419+LBargie@users.noreply.github.com>
2023-02-11 10:35:32 +0000

    Update API codebase

--- api/monkeypox_api.py
+++ api/monkeypox_api.py
@@ -1,27 +1,36 @@
-from typing import List
-from fastapi import FastAPI, Query, Depends, Request, status
+from typing import List, Optional, Type
+from fastapi import FastAPI, Query, Request, status, Depends
 from fastapi.encoders import jsonable_encoder
-from fastapi.exceptions import RequestValidationError
+from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel, BaseConfig, Field
 from scraper.scrape import MonkeypoxScraper
+import re
+
+
+def date_checker(date: str) -> HTTPException:
+
+    if not re.fullmatch("\d{4}-\d{2}-\d{2}", date):
+        raise HTTPException(status_code=422, detail="Search date must be in the format YYYY-MM-DD")
+
+def range_checker(start_date: str, end_date: str) -> HTTPException:
+
+    if not (re.fullmatch("\d{4}-\d{2}-\d{2}", start_date) and re.fullmatch("\d{4}-\d{2}-\d{2}", end_date)):
+        raise HTTPException(status_code=422, detail="Search date must be in the format YYYY-MM-DD")
 
 
 app = FastAPI()
 
-@app.exception_handler(RequestValidationError)
-async def validation_exception_handler(request: Request, exc: RequestValidationError):
-    return JSONResponse(
-        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
-        content=jsonable_encoder({"error": "Search date must be in the format YYYY-MM-DD"}),
-    )
+# create MonkeypoxScraper object
+scraper = MonkeypoxScraper()
 
 @app.get("/")
 async def root():
     return {"message": "Welcome to the UK monkeypox data API"}
 
 @app.get("/info")
-async def available_data(scraper: MonkeypoxScraper = Depends(MonkeypoxScraper)):
-    
+async def available_data():
+
     available_data = scraper.available_data()
 
     for k, v in available_data.items():
@@ -30,7 +39,7 @@ async def available_data(scraper: MonkeypoxScraper = Depends(MonkeypoxScraper)):
     return available_data
 
 @app.get("/latestdata/")
-async def latest_data(scraper: MonkeypoxScraper = Depends(MonkeypoxScraper)):
+async def latest_data():
 
     data = scraper.available_data()
 
@@ -38,34 +47,19 @@ async def latest_data(scraper: MonkeypoxScraper = Depends(MonkeypoxScraper)):
 
     return scraper.search_by_date(date=date)
 
-@app.get("/search_by_date/")
-async def search_by_date(
-    search_date: str = Query(regex="\d{4}-\d{2}-\d{2}"), 
-    scraper: MonkeypoxScraper= Depends(MonkeypoxScraper)
-    ):
+@app.get("/search_by_date/{date}", dependencies=[Depends(date_checker)])
+async def search_by_date(date: str):
 
-    return scraper.search_by_date(date=search_date)
+    return scraper.search_by_date(date=date)
 
-@app.get("/search_multiple/")
-async def search_multiple(
-    search_dates: List[str] = Query(regex="\d{4}-\d{2}-\d{2}"),
-    scraper: MonkeypoxScraper= Depends(MonkeypoxScraper)
-    ):
-
-    return scraper.search_multiple_dates(dates=search_dates)
-
-@app.get("/search_range/")
-async def search_range(
-    start_date: str = Query(regex="\d{4}-\d{2}-\d{2}"), 
-    end_date: str = Query(regex="\d{4}-\d{2}-\d{2}"),
-    scraper: MonkeypoxScraper = Depends(MonkeypoxScraper)
-    ):
+@app.get("/search_range/{start_date}/{end_date}", dependencies=[Depends(range_checker)])
+async def search_range(start_date: str, end_date: str):
 
     dates = [start_date, end_date]
 
     return scraper.search_date_range(date_range=dates)
 
 @app.get("/alldata")
-async def search_all_dates(scraper: MonkeypoxScraper=Depends(MonkeypoxScraper)):
+async def search_all_dates():
 
     return scraper.search_all_dates()
