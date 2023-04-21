ac0c7b5c4486f1549aa4211e8c3b9ef4ad455b4f
Lee Barrett <90680419+LBargie@users.noreply.github.com>
2023-04-21 15:12:25 +0100

    scraper class object call to top

--- api/monkeypox_api.py
+++ api/monkeypox_api.py
@@ -4,15 +4,19 @@ from fastapi.encoders import jsonable_encoder
 from fastapi.exceptions import RequestValidationError, HTTPException
 from fastapi.responses import JSONResponse
 from pydantic import BaseModel, BaseConfig, Field
-from scraper.scrape import MonkeypoxScraper
+from .scraper.scrape import MonkeypoxScraper
 import re
 
+# create MonkeypoxScraper object
+scraper = MonkeypoxScraper()
 
+# function for checking the date format
 def date_checker(date: str) -> HTTPException:
 
     if not re.fullmatch("\d{4}-\d{2}-\d{2}", date):
         raise HTTPException(status_code=422, detail="Search date must be in the format YYYY-MM-DD")
 
+# function for checking the date format in range endpoint
 def range_checker(start_date: str, end_date: str) -> HTTPException:
 
     if not (re.fullmatch("\d{4}-\d{2}-\d{2}", start_date) and re.fullmatch("\d{4}-\d{2}-\d{2}", end_date)):
@@ -21,8 +25,6 @@ def range_checker(start_date: str, end_date: str) -> HTTPException:
 
 app = FastAPI()
 
-# create MonkeypoxScraper object
-scraper = MonkeypoxScraper()
 
 @app.get("/")
 async def root():
